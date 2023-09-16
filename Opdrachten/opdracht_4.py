ceea51ca2108215379672fa3ccb353e238a9ecc4
Max <max.groenendijk@student.hu.nl>
2023-09-16 12:48:02 +0200

    opdracht 4 afgerond

---
+++ Opdrachten/opdracht_4.py
@@ -0,0 +1,78 @@
+list_populair_games_2021 = ["Player’s Unknown Battle Ground (PUBG) 50 Million 2018",
+"Fortnite Battle Royale 39 Million 2017",
+"Apex Legends 50 Million (1 Month) 2019",
+"Leauge of Legends (LOL) 27 Million 2009",
+"Counter Strike; Global Offensive 32 Million 2014",
+"Heartstone 29 Million 20120",
+"Minecraft 91 Million 2011",
+"DOTA 2 5 million 2015",
+"The Division 2 N/A 2019",
+"The Splatoon 2"]
+
+# deel 1
+# print(list_populair_games_2021[4])
+# print(len(list_populair_games_2021[7]))
+# amount_in_list_populair_games_2021 = len(list_populair_games_2021)
+
+# print(f'There are {amount_in_list_populair_games_2021} games in the list')
+
+# list_populair_games_2021.insert(1, "Snake")
+# print(list_populair_games_2021)
+# print(f' Sadly the game {list_populair_games_2021[10]} is no longer in the top 10 list , we say our honorable goodbye to the game {list_populair_games_2021.pop()}')
+# print(list_populair_games_2021)
+
+
+# l_replace = [s.replace("20120", "2012") for s in list_populair_games_2021]
+# print(l_replace)
+
+#deel 2
+pc_leveranciers = ("Apple",
+               "Asus",
+               "Dell", 
+               "Lenovo",
+               "Acer",
+               "Samsung",
+               "MSI", 
+               "Hewlett-Packard (HP)", 
+               "Toshiba", 
+               "Microsoft", 
+               "Chuwi", 
+               "Sony")
+# print(len(pc_leveranciers))
+# print(f'De tuple bevat {len(pc_leveranciers)} computer leveranciers')
+# print(f'De naam van {pc_leveranciers[7]} bestaat uit {len(pc_leveranciers[7])} karakters')
+# print(pc_leveranciers[11])
+
+#deel 3
+# my_dict = {"The Simpsons": "636-555-3226",
+#            "Vegas Vacation": "555-0100",
+#            "Ghostbusters" : "555-23678",
+#            "Billy Madison" : "555-0840",
+#            "Swingers" : "213-555-4679",
+#            "Bruce Almighty" : "555-0123",
+#            "Seinfeld" : "555-FILK",
+#            "Arrested Development" : "555-0113",
+#            "Die Hard With a Vengeance" :"555-0001",
+#            "The A-Team" :"555-6162"}
+# print(f'The telephone number of Bruce Almighty is {my_dict["Bruce Almighty"]}')
+# my_dict["Harry Potter"] = "605-475-6961"
+# change_telephone_number = input("Who's telephone number do you want to change?")
+# changed_telephone_number = input("What is the new telephone number?")
+
+# my_dict[change_telephone_number] = changed_telephone_number
+
+# print(f'the telephone number of {change_telephone_number} is changed to {changed_telephone_number}')
+# print(my_dict)
+
+# delete_telephone_number = input("Who's telephone number do you want to delete?")
+
+# print(f'Telephonenumber {my_dict.pop(delete_telephone_number)} from {delete_telephone_number} has been deleted')
+
+# print(my_dict)
+# print(f'In the dictionairy are {len(my_dict)} telephone numbers')
+
+
+
+
+
+
